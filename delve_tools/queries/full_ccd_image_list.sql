979ef2d8b7edba63d5ae3ad09edd3477ee11e41d
Eric Morganson <ericm@illinois.edu>
2019-08-20 17:43:13 -0700

    Update full_ccd_image_list.sql
    Added relative path

--- delve_tools/queries/full_ccd_image_list.sql
+++ delve_tools/queries/full_ccd_image_list.sql
@@ -1,30 +1,31 @@
 -- Query to generate table of all CCDs
+-- paths should be appended to 'http://decade.ncsa.illinois.edu/deca_archive/'
 with images as (
-  select distinct i.expnum, i.ccdnum, i.band, racmin, racmax, deccmin, deccmax, i.filename as ilink 
+  select distinct i.expnum, i.ccdnum, i.band, racmin, racmax, deccmin, deccmax, fai.path||'/'||i.filename||fai.compression as ilink as ilink 
   from decade.image i, decade.file_archive_info fai 
   where i.filetype='red_immask' and i.filename=fai.filename 
   order by expnum, ccdnum
 ),  
 catalogs as (
-  select distinct c.expnum, c.ccdnum, c.filename as clink 
+  select distinct c.expnum, c.ccdnum, fai.path||'/'||c.filename||fai.compression as clink 
   from decade.catalog c, decade.file_archive_info fai 
   where c.filetype='cat_finalcut' and c.filename=fai.filename 
   order by expnum, ccdnum
 ), 
 psfs as (
-  select distinct m.expnum, m.ccdnum, m.filename as plink 
+  select distinct m.expnum, m.ccdnum, fai.path||'/'||m.filename||fai.compression as plink 
   from decade.miscfile m, decade.file_archive_info fai 
   where m.filetype='psfex_model' and m.filename=fai.filename 
   order by expnum, ccdnum
 ),
 backgrounds as (
-  select distinct i.expnum, i.ccdnum, i.filename as blink 
+  select distinct i.expnum, i.ccdnum, fai.path||'/'||i.filename||fai.compression as blink 
   from decade.image i, decade.file_archive_info fai 
   where i.filetype='red_bkg' and i.filename=fai.filename 
   order by expnum, ccdnum
 ),  
 segmaps as (
-  select distinct m.expnum, m.ccdnum, m.filename as slink 
+  select distinct m.expnum, m.ccdnum, fai.path||'/'||m.filename||fai.compression as slink 
   from decade.miscfile m, decade.file_archive_info fai 
   where m.filetype='red_segmap' and m.filename=fai.filename 
   order by expnum, ccdnum
